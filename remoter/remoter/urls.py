819001c2704bf580cc98eaaadfb443038c93233b
hedge31 <hedge31@qq.com>
2022-09-29 23:41:42 +0800

    new

--- remoter/remoter/urls.py
+++ remoter/remoter/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import remoter.views as remoter_views
 
 urlpatterns = [
+    path('',remoter_views.index),
     path('admin/', admin.site.urls),
+    
 ]
